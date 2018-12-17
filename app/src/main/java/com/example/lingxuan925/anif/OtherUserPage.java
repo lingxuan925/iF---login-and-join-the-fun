ea195d5917ba1814749af8930da68c752a15f586
Unknown <zhatiayua59@gmail.com>
2018-12-17 00:54:06 -0500

    nothing new

--- app/src/main/java/com/example/lingxuan925/anif/OtherUserPage.java
+++ app/src/main/java/com/example/lingxuan925/anif/OtherUserPage.java
@@ -10,8 +10,6 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class OtherUserPage extends AppCompatActivity {
